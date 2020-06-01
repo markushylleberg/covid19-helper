b7fcf3b604f6eb50c89b01df793d82973e636df4
Markus Hylleberg <markushylleberg@gmail.com>
2020-06-01 19:06:35 +0200

    fixed knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,37 +1,26 @@
 const knexSnakeCaseMapper = require('objection').knexSnakeCaseMappers;
-const credentials = require('./config/dbcredentials.js');
+// const credentials = require('./config/dbcredentials.js');
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      user: credentials.user,
-      password: credentials.password,
-      database: credentials.database,
-      charset: 'utf8',
-      timezone: 'utc+2',
-    },
-  },
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     user: credentials.user,
+  //     password: credentials.password,
+  //     database: credentials.database,
+  //     charset: 'utf8',
+  //     timezone: 'utc+2',
+  //   }
+  //   },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL, searchPath=['public']
+    }
+  
   ...knexSnakeCaseMapper(),
 
   // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password',
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //   },
-  // },
-
-  // production: {
   //   client: 'postgresql',
   //   connection: {
   //     database: 'my_db',
