df17ffeed1088bb6cf1b695ab528059e29a3973b
Markus Hylleberg <markushylleberg@gmail.com>
2020-06-01 21:18:44 +0200

    new change

--- server/app.js
+++ server/app.js
@@ -46,7 +46,7 @@ app.use(usersRoute);
 app.use(shiftsRoute);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, 'client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
