1935ce7e17c7b4fa5641de426c916dec0849a139
Markus Hylleberg <markushylleberg@gmail.com>
2020-06-01 20:34:24 +0200

    new change

---
+++ client/build/precache-manifest.eac59dc5cf78d125bca6db7fcc8df8a6.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b8ffccbe4168ddaccbbd167c4e207c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bac23b2c692324a6ca71",
+    "url": "/static/css/main.bfdb1fea.chunk.css"
+  },
+  {
+    "revision": "4f5759fd01128aa38e96",
+    "url": "/static/js/2.52d365e2.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.52d365e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac23b2c692324a6ca71",
+    "url": "/static/js/main.84f63b92.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "e148f969ae609dee2500cb42fab42b75",
+    "url": "/static/media/AI.e148f969.png"
+  },
+  {
+    "revision": "85bd075f1e9253e1fb5142ec7ad511b9",
+    "url": "/static/media/AO.85bd075f.png"
+  },
+  {
+    "revision": "41774c45ade097644aab696ecd976f92",
+    "url": "/static/media/AS.41774c45.png"
+  },
+  {
+    "revision": "dff87541c31b85adea545328c20e7fe8",
+    "url": "/static/media/AU.dff87541.png"
+  },
+  {
+    "revision": "1dd9f1b9cafeec9a21088a2c6b5c879a",
+    "url": "/static/media/BA.1dd9f1b9.png"
+  },
+  {
+    "revision": "12f3446471046489b51ffd7270308aab",
+    "url": "/static/media/BI.12f34464.png"
+  },
+  {
+    "revision": "8ba5472535d18bd51b96812e94661fb8",
+    "url": "/static/media/BN.8ba54725.png"
+  },
+  {
+    "revision": "d69bd7a3d055e427bccd84678e16af26",
+    "url": "/static/media/BV.d69bd7a3.png"
+  },
+  {
+    "revision": "057d8793ccf43bebf1f33cdfee03d84f",
+    "url": "/static/media/BZ.057d8793.png"
+  },
+  {
+    "revision": "7808a560742a213f4ab2d4491efca82a",
+    "url": "/static/media/CC.7808a560.png"
+  },
+  {
+    "revision": "d5c57c22e4817590816e5cf60994273c",
+    "url": "/static/media/CK.d5c57c22.png"
+  },
+  {
+    "revision": "587fe4b897d47e5578818b7269780e01",
+    "url": "/static/media/CX.587fe4b8.png"
+  },
+  {
+    "revision": "7e23b11e2f653b5926a762826b8768ab",
+    "url": "/static/media/DM.7e23b11e.png"
+  },
+  {
+    "revision": "2edef71766de6d925ef8735a8731c579",
+    "url": "/static/media/ET.2edef717.png"
+  },
+  {
+    "revision": "6e3621fa9b348ebf1953762fd0c43351",
+    "url": "/static/media/FK.6e3621fa.png"
+  },
+  {
+    "revision": "e21327386c16b1635d47a8a5deff0dec",
+    "url": "/static/media/GB.e2132738.png"
+  },
+  {
+    "revision": "7fe18b245f52c5f7fe55b685356736d2",
+    "url": "/static/media/GD.7fe18b24.png"
+  },
+  {
+    "revision": "5504d3f2ae16d7bfdb562ce7ed5147c9",
+    "url": "/static/media/GU.5504d3f2.png"
+  },
+  {
+    "revision": "9a8202c1156d199eadd9e25a0a2cff7a",
+    "url": "/static/media/IO.9a8202c1.png"
+  },
+  {
+    "revision": "91a0b3e6de63522b4e7cf869eb611084",
+    "url": "/static/media/KE.91a0b3e6.png"
+  },
+  {
+    "revision": "da711adc03ba86872af5230036743bd7",
+    "url": "/static/media/KG.da711adc.png"
+  },
+  {
+    "revision": "8b496cf6e27f0eb1a0eb8c63028c1a7d",
+    "url": "/static/media/KI.8b496cf6.png"
+  },
+  {
+    "revision": "182a6d44849165173d78261a8d05533e",
+    "url": "/static/media/KY.182a6d44.png"
+  },
+  {
+    "revision": "61d14219ba4606216a81a98ae3b7ac91",
+    "url": "/static/media/MH.61d14219.png"
+  },
+  {
+    "revision": "40cec3100356425ffcd3b739dbd86d26",
+    "url": "/static/media/MO.40cec310.png"
+  },
+  {
+    "revision": "62694f2551495e09496ffe9e361f9203",
+    "url": "/static/media/MP.62694f25.png"
+  },
+  {
+    "revision": "cd9e8f7d2e97fd8da0fe1d7dda5b40d9",
+    "url": "/static/media/MQ.cd9e8f7d.png"
+  },
+  {
+    "revision": "15bbcc3ae171ae278d43b919b002c802",
+    "url": "/static/media/MS.15bbcc3a.png"
+  },
+  {
+    "revision": "a38cb28136330d2a9aa11a7c4585d11e",
+    "url": "/static/media/NP.a38cb281.png"
+  },
+  {
+    "revision": "cc814a57aef02d5668935d7cb9497fe5",
+    "url": "/static/media/NZ.cc814a57.png"
+  },
+  {
+    "revision": "134b112dbad695db2d9a30f386f05990",
+    "url": "/static/media/PG.134b112d.png"
+  },
+  {
+    "revision": "13f8f63867d1a6906b2e129439d1f5dc",
+    "url": "/static/media/PH.13f8f638.png"
+  },
+  {
+    "revision": "fb3a778cafb4ead2503c61875a45cbf6",
+    "url": "/static/media/PK.fb3a778c.png"
+  },
+  {
+    "revision": "89cad3b2e1dd02b5819d5842fb8d3ed0",
+    "url": "/static/media/PN.89cad3b2.png"
+  },
+  {
+    "revision": "134972a183f6bf6f265ef181688f2d23",
+    "url": "/static/media/SC.134972a1.png"
+  },
+  {
+    "revision": "9ac286287e8c5cfe21e5d3cf51562d65",
+    "url": "/static/media/SM.9ac28628.png"
+  },
+  {
+    "revision": "b545f862c82d11518c66dbec71f34d18",
+    "url": "/static/media/TC.b545f862.png"
+  },
+  {
+    "revision": "eb0afcb6a9ecebcfa54ca4a2542870ea",
+    "url": "/static/media/TK.eb0afcb6.png"
+  },
+  {
+    "revision": "337798c62532002d967ed717086f7144",
+    "url": "/static/media/TM.337798c6.png"
+  },
+  {
+    "revision": "1e2841e1f48780cfc61fe9aef15dc651",
+    "url": "/static/media/TN.1e2841e1.png"
+  },
+  {
+    "revision": "3386158ffba5b99be66b4b7561c0f0d4",
+    "url": "/static/media/TT.3386158f.png"
+  },
+  {
+    "revision": "5181ed063f0d0488355540688c8d0e6c",
+    "url": "/static/media/US.5181ed06.png"
+  },
+  {
+    "revision": "8d237fbb2ae737b7b46e92f912a33bfc",
+    "url": "/static/media/VU.8d237fbb.png"
+  },
+  {
+    "revision": "e8a351ecb1e7fcde12a76851feececa5",
+    "url": "/static/media/bonaire.e8a351ec.png"
+  },
+  {
+    "revision": "882fbcf0cdbc28d7105e712085de3ef4",
+    "url": "/static/media/british-columbia.882fbcf0.png"
+  },
+  {
+    "revision": "b0c1ad5843275d7ce27301304525f7e2",
+    "url": "/static/media/corsica.b0c1ad58.png"
+  },
+  {
+    "revision": "247b380e5a195dcbcf3f454700d8b116",
+    "url": "/static/media/east-timor.247b380e.png"
+  },
+  {
+    "revision": "376137e79185307b120d986de14a782a",
+    "url": "/static/media/european-union.376137e7.png"
+  },
+  {
+    "revision": "2f917120bbe64b94289eb124e1441add",
+    "url": "/static/media/otan.2f917120.png"
+  },
+  {
+    "revision": "d9dcc52435ddf3889f79124b64e9cc55",
+    "url": "/static/media/republic-of-macedonia.d9dcc524.png"
+  },
+  {
+    "revision": "0a136d94001be4dfe3d9f3b85647dba8",
+    "url": "/static/media/sicily.0a136d94.png"
+  },
+  {
+    "revision": "66e8fb7812541ce910e2903c485e9d54",
+    "url": "/static/media/sint-eustatius.66e8fb78.png"
+  },
+  {
+    "revision": "7992881bc761bf1e0dad924d61ff3758",
+    "url": "/static/media/south-korea.7992881b.png"
+  },
+  {
+    "revision": "bb07711db7806ba6e0178029c082c2ba",
+    "url": "/static/media/tibet.bb07711d.png"
+  },
+  {
+    "revision": "a92404a3e654d0569db1b703456b4c56",
+    "url": "/static/media/tubalu.a92404a3.png"
+  },
+  {
+    "revision": "d5fb92ff7055849aa9f649e1fccfa80a",
+    "url": "/static/media/united-nations.d5fb92ff.png"
+  }
+]);
--- server/knexfile.js
+++ server/knexfile.js
@@ -1,38 +1,57 @@
 // const knexSnakeCaseMapper = require('objection').knexSnakeCaseMappers;
 // const credentials = require('./config/dbcredentials.js');
 
-module.exports = {
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     host: '127.0.0.1',
-  //     user: credentials.user,
-  //     password: credentials.password,
-  //     database: credentials.database,
-  //     charset: 'utf8',
-  //     timezone: 'utc+2',
-  //   }
-  //   },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-  },
+const options =
+  process.env.NODE_ENV === 'production'
+    ? {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        searchPath: ['public'],
+      }
+    : {
+        client: 'pg',
+        connection: {
+          host: 'localhost',
+          database: 'covid19',
+        },
+      };
 
-  // ...knexSnakeCaseMapper(),
+const knex = require('knex')(options);
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password',
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //   },
-  // },
-};
+module.exports = knex;
+
+// module.exports = {
+// development: {
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: credentials.user,
+//     password: credentials.password,
+//     database: credentials.database,
+//     charset: 'utf8',
+//     timezone: 'utc+2',
+//   }
+//   },
+// production: {
+//   client: 'pg',
+//   connection: process.env.DATABASE_URL,
+// },
+
+// ...knexSnakeCaseMapper(),
+
+// staging: {
+//   client: 'postgresql',
+//   connection: {
+//     database: 'my_db',
+//     user: 'username',
+//     password: 'password',
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10,
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//   },
+// },
+// };
